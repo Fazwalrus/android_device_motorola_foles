d1715ba0dcfdc0d37827e5c05dfea834f07e6c9f
Rohan Hasabe <rohanhasabe8@gmail.com>
2021-06-17 20:00:14 +0200

    parker: Remove unused kernel module
    Change-Id: I1093090461b58b3f9d56bd1391a9542bd85f127b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,6 @@ TARGET_KERNEL_CONFIG := vendor/parker_defconfig
 # Kernel modules - Audio
 TARGET_MODULE_ALIASES += \
     snd-soc-aov-trigger.ko:aov_trigger.ko \
-    snd-soc-cs35l41-spi.ko:cirrus_cs35l41-spi.ko \
     snd-soc-cs35l41.ko:cirrus_cs35l41.ko \
     snd-soc-cs47l35.ko:cirrus_cs47l35.ko \
     snd-soc-madera.ko:cirrus_madera.ko \
